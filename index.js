ca84fb399bacfeed3ac9da2189c4d5a3de4de767
Jordan Boudreau <jordboudreau@gmail.com>
2019-09-20 20:08:31 -0700

    node server

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const app = require('express')()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 8081
+const routes = require('./routes/index')
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/api', routes);
+
+//server
+app.listen(port, () => {
+    console.log('App is running on port ' + port);
+})
